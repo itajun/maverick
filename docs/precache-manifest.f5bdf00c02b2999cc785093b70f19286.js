481edabec8c4b74a7d8fab0bbad828faa8d8c546
Itamar Vieira Junior <itajun@gmail.com>
2022-07-13 08:51:08 +1000

    Yet another

---
+++ docs/precache-manifest.f5bdf00c02b2999cc785093b70f19286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0045f84e978858adbf77",
+    "url": "/itajun/maverick/static/css/main.b205ec79.chunk.css"
+  },
+  {
+    "revision": "0045f84e978858adbf77",
+    "url": "/itajun/maverick/static/js/main.165d9a8d.chunk.js"
+  },
+  {
+    "revision": "9220dd10e052141ab00e",
+    "url": "/itajun/maverick/static/js/runtime~main.5a026af2.js"
+  },
+  {
+    "revision": "37e69c3860c93a92eeb8",
+    "url": "/itajun/maverick/static/js/2.7dec5055.chunk.js"
+  },
+  {
+    "revision": "8ccd95e250a1073ff66c",
+    "url": "/itajun/maverick/static/js/3.c0dfc813.chunk.js"
+  },
+  {
+    "revision": "cb0af2947c7791cccdd8cfc0533d31ca",
+    "url": "/itajun/maverick/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a61d587106f5a6f099d39cbe5a2f9ab2.js"
+  "/itajun/maverick/precache-manifest.f5bdf00c02b2999cc785093b70f19286.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/itajun/maverick/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
